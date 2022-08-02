8f1cb5afb211c790a8299c6ea8bf7984408b337a
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-02 20:33:52 +0300

    added task5

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 4
-        float targetWeight = 7f;
-        float minStep = 0.25f;
-        float maxStep = 0.5f;
-        float maxDayCount = targetWeight / minStep;
-        float minDayCount = targetWeight / maxStep;
-        float averageDayCount = (maxDayCount + minDayCount) / 2f;
-        System.out.println("Задача 4");
-        System.out.println("Если спортсмен будет терять по " + minStep + " кг в день, то потребуется " + maxDayCount + " дней");
-        System.out.println("Если спортсмен будет терять по " + maxStep + " кг в день, то потребуется " + minDayCount + " дней");
-        System.out.println("В среднем потребуется " + averageDayCount + " день");
+        //Задача 5
+        float salaryMasha = 67760f;
+        float salaryDen = 83690f;
+        float salaryKris = 76230f;
+        float salaryCoefficient = 1.1f;
+        float newSalaryMasha = salaryMasha * salaryCoefficient;
+        float newSalaryDen = salaryDen * salaryCoefficient;
+        float newSalaryKris = salaryKris * salaryCoefficient;
+        byte monthsCount = 12;
+        float annualSalaryMasha = salaryMasha * monthsCount;
+        float annualSalaryDen = salaryDen * monthsCount;
+        float annualSalaryKris = salaryKris * monthsCount;
+        float newAnnualSalaryMasha = newSalaryMasha * monthsCount;
+        float newAnnualSalaryDen = newSalaryDen * monthsCount;
+        float newAnnualSalaryKris = newSalaryKris * monthsCount;
+        float incomeMasha = newAnnualSalaryMasha - annualSalaryMasha;
+        float incomeDen = newAnnualSalaryDen - annualSalaryDen;
+        float incomeKris = newAnnualSalaryKris - annualSalaryKris;
+        System.out.println("Задача 5");
+        System.out.println("Маша теперь получает " + newSalaryMasha + " руб. Годовой доход вырос на " + incomeMasha + " рублей");
+        System.out.println("Денис теперь получает " + newSalaryDen + " руб. Годовой доход вырос на " + incomeDen + " рублей");
+        System.out.println("Кристина теперь получает " + newSalaryKris + " руб. Годовой доход вырос на " + incomeKris + " рублей");
     }
 }

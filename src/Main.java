ab75fd10eec88ae70cf47d3c8046634245353168
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-02 20:17:46 +0300

    added task2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
-        byte month = 8;
-        short day = 12;
-        int year = 1995;
-        long phoneNumber = 9776122425L;
-        float temperature = -4.567f;
-        double e = 2.718281828d;
-        char symbol = 'a';
-        boolean isYearEven = (year % 2) == 0;
+        //Задача 2
+        float boxerWeight1 = 78.2f; //вес первого боксера в кг
+        float boxerWeight2 = 82.7f; //вес второго боксера в кг
+        float totalWeight = boxerWeight1 + boxerWeight2; //суммарный вес
+        float difWeight = boxerWeight2 - boxerWeight1; //разница между весами
+        System.out.println("Задача 2");
+        System.out.println("Вес первого бойца = " + boxerWeight1 + "кг");
+        System.out.println("Вес второго бойца = " + boxerWeight2 + "кг");
+        System.out.println("Общий вес двух бойцов = " +  totalWeight + "кг");
+        System.out.println("Разница между весами бойцов = " +   difWeight + "кг");
     }
 }

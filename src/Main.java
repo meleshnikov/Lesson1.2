4c8cdb67b9087c0025124752cb104be48a45b4d0
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-02 20:15:00 +0300

    added task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lesson1.2");
+        //Задача 1
+        byte month = 8;
+        short day = 12;
+        int year = 1995;
+        long phoneNumber = 9776122425L;
+        float temperature = -4.567f;
+        double e = 2.718281828d;
+        char symbol = 'a';
+        boolean isYearEven = (year % 2) == 0;
     }
 }

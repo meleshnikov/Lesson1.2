cff99655271df77a4b944f7b75abf007cede7849
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-02 20:29:02 +0300

    added task3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 2
-        float boxerWeight1 = 78.2f; //вес первого боксера в кг
-        float boxerWeight2 = 82.7f; //вес второго боксера в кг
-        float totalWeight = boxerWeight1 + boxerWeight2; //суммарный вес
-        float difWeight = boxerWeight2 - boxerWeight1; //разница между весами
-        System.out.println("Задача 2");
-        System.out.println("Вес первого бойца = " + boxerWeight1 + "кг");
-        System.out.println("Вес второго бойца = " + boxerWeight2 + "кг");
-        System.out.println("Общий вес двух бойцов = " +  totalWeight + "кг");
-        System.out.println("Разница между весами бойцов = " +   difWeight + "кг");
+        //Задача 3
+        float bananaWeight = 80f; //масса одного банана в г
+        float milkDensity = 105f/100f; //плотность молока в г/мл
+        float iceCreamWeight = 100f; //масса одного мороженного в г
+        float eggWeight = 70f; //масса одного яйца в г
+        float milkVolume = 200f; //объем молока в мл
+        byte bananaQty = 5; //Количество бананов
+        byte iceCreamQty = 2; //количество мороженных
+        byte eggsQty = 1; //количество яиц
+        float breakfastWeight = bananaWeight * bananaQty + milkDensity*milkVolume + iceCreamWeight * iceCreamQty
+                + eggWeight * eggsQty; //общая масса завтрака в г
+        short grPerKg = 1000;
+        float breakfastWeightKg = breakfastWeight / (float)grPerKg;
+        System.out.println("Задача 3");
+        System.out.println("Масса завтрака составляет " + breakfastWeight + "г (" + breakfastWeightKg + "кг)");
     }
 }

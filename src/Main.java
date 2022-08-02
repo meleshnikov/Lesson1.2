1cdc340d3df036379ea26927a6f98059abcea4b4
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-02 20:31:56 +0300

    added task4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 3
-        float bananaWeight = 80f; //масса одного банана в г
-        float milkDensity = 105f/100f; //плотность молока в г/мл
-        float iceCreamWeight = 100f; //масса одного мороженного в г
-        float eggWeight = 70f; //масса одного яйца в г
-        float milkVolume = 200f; //объем молока в мл
-        byte bananaQty = 5; //Количество бананов
-        byte iceCreamQty = 2; //количество мороженных
-        byte eggsQty = 1; //количество яиц
-        float breakfastWeight = bananaWeight * bananaQty + milkDensity*milkVolume + iceCreamWeight * iceCreamQty
-                + eggWeight * eggsQty; //общая масса завтрака в г
-        short grPerKg = 1000;
-        float breakfastWeightKg = breakfastWeight / (float)grPerKg;
-        System.out.println("Задача 3");
-        System.out.println("Масса завтрака составляет " + breakfastWeight + "г (" + breakfastWeightKg + "кг)");
+        //Задача 4
+        float targetWeight = 7f;
+        float minStep = 0.25f;
+        float maxStep = 0.5f;
+        float maxDayCount = targetWeight / minStep;
+        float minDayCount = targetWeight / maxStep;
+        float averageDayCount = (maxDayCount + minDayCount) / 2f;
+        System.out.println("Задача 4");
+        System.out.println("Если спортсмен будет терять по " + minStep + " кг в день, то потребуется " + maxDayCount + " дней");
+        System.out.println("Если спортсмен будет терять по " + maxStep + " кг в день, то потребуется " + minDayCount + " дней");
+        System.out.println("В среднем потребуется " + averageDayCount + " день");
     }
 }
